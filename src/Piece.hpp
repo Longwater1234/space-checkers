be8b8c6b7d306cc6e347ad841694188dada8ac4f
Davis Tibbz <davisinyo@gmail.com>
2024-12-16 19:36:11 +0300

    fixed #129

--- src/Piece.hpp
+++ src/Piece.hpp
@@ -25,7 +25,7 @@ class Piece final : public sf::Drawable, public sf::Transformable
 {
 
   public:
-    Piece(const sf::CircleShape &circle, const PieceType &pType, short id);
+    explicit Piece(const sf::CircleShape &circle, const PieceType &pType, short id);
     Piece() = delete;
     Piece(const Piece &) = delete;
     Piece &operator=(const Piece &) = delete;
