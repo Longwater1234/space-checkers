4aa8ab9ee0b2ede5617f53bfe5227658ec0f5d72
Davis Tibbz <davisinyo@gmail.com>
2025-04-17 17:22:51 +0300

    download mbedtls as tar.gz

--- dependencies/simdjson.cmake
+++ dependencies/simdjson.cmake
@@ -1,7 +1,7 @@
 # Get simdjson 3.10.0
 CPMAddPackage(
   NAME simdjson
-  URL  https://github.com/simdjson/simdjson/archive/refs/tags/v3.10.0.tar.gz
+  URL  "https://github.com/simdjson/simdjson/archive/refs/tags/v3.10.0.tar.gz"
   VERSION 3.10.0
   DOWNLOAD_EXTRACT_TIMESTAMP TRUE
 ) 
