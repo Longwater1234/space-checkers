1fb379a1b22ba1943fe27637a621505f7881d388
Davis Tibbz <davisinyo@gmail.com>
2026-03-14 10:32:37 +0300

    use curly braces for ctor's

--- tests/PieceTests.cpp
+++ tests/PieceTests.cpp
@@ -4,27 +4,27 @@
 
 TEST(PieceTests, GetId_ReturnsCorrectId)
 {
-    sf::CircleShape circle(37.5f);
+    sf::CircleShape circle{37.5f};
     circle.setPosition(0.0f, 0.0f);
-    chk::Piece piece(circle, chk::PieceType::Red, 42);
+    chk::Piece piece{circle, chk::PieceType::Red, 42};
     EXPECT_EQ(piece.getId(), 42);
 }
 
 TEST(PieceTests, NewPiece_IsNotKing)
 {
-    sf::CircleShape circle(37.5f);
+    sf::CircleShape circle{37.5f};
     circle.setPosition(0.0f, 0.0f);
-    chk::Piece piece(circle, chk::PieceType::Red, 1);
+    chk::Piece piece{circle, chk::PieceType::Red, 1};
     EXPECT_FALSE(piece.getIsKing());
 }
 
 TEST(PieceTests, MoveSimple_ValidRedMove_ReturnsTrue)
 {
-    sf::CircleShape circle(37.5f);
+    sf::CircleShape circle{37.5f};
     circle.setPosition(150.0f, 150.0f);
-    chk::Piece piece(circle, chk::PieceType::Red, 1);
+    chk::Piece piece{circle, chk::PieceType::Red, 1};
 
     // RED piece moves up-left by one cell (deltaX=-75, deltaY=-75)
-    sf::Vector2f dest(75.0f, 75.0f);
+    sf::Vector2f dest{75.0f, 75.0f};
     EXPECT_TRUE(piece.moveSimple(dest));
 }
--- tests/PlayerTests.cpp
+++ tests/PlayerTests.cpp
@@ -4,24 +4,24 @@
 
 TEST(PlayerTests, CaptureEnemyWith_Success)
 {
-    chk::Player player(chk::PlayerType::PLAYER_RED);
+    chk::Player player{chk::PlayerType::PLAYER_RED};
 
     // Create a RED piece positioned 2 cells diagonally from destination
-    sf::CircleShape circle(37.5f);
+    sf::CircleShape circle{37.5f};
     circle.setPosition(150.0f, 150.0f);
     chk::PiecePtr piece = std::make_unique<chk::Piece>(circle, chk::PieceType::Red, 1);
     player.receivePiece(piece);
 
     // RED piece captures by jumping 2 cells diagonally upward (deltaX=-150, deltaY=-150)
-    sf::Vector2f destination(0.0f, 0.0f);
+    sf::Vector2f destination{0.0f, 0.0f};
     EXPECT_TRUE(player.captureEnemyWith(1, destination));
 }
 
 TEST(PlayerTests, LosePiece_RemovesPieceFromBasket)
 {
-    chk::Player player(chk::PlayerType::PLAYER_RED);
+    chk::Player player{chk::PlayerType::PLAYER_RED};
 
-    sf::CircleShape circle(37.5f);
+    sf::CircleShape circle{37.5f};
     circle.setPosition(0.0f, 0.0f);
     chk::PiecePtr piece = std::make_unique<chk::Piece>(circle, chk::PieceType::Red, 1);
     player.receivePiece(piece);
