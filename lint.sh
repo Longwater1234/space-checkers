8a0674076a87f20f37c63a6423d5450cffc1bb96
Davis Tibbz <davisinyo@gmail.com>
2026-04-05 17:55:07 +0300

    include test dir in code formatting

--- lint.sh
+++ lint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# source directory
+# target directory
 SRC_DIR="src"
+TEST_DIR="tests"
 
-clang-format $(find $SRC_DIR -type f -name "*.cpp" -o -name "*.hpp") -i --verbose
+clang-format $(find $SRC_DIR $TEST_DIR -type f -name "*.cpp" -o -name "*.hpp") -i --verbose
 echo "Formatting completed."
